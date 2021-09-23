26cc9a86b662a9d0b5efdeb1b89969d50c7138be
Tiago Silva de Oliveira <tiagobsb84@gmail.com>
2021-09-23 16:37:49 -0300

    Adicionando classe Order no Repository, Service e Resource

--- src/main/java/com/tiago/entities/Order.java
+++ src/main/java/com/tiago/entities/Order.java
@@ -19,6 +19,7 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private Instant moment;
 	
 	@ManyToOne
---
+++ src/main/java/com/tiago/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.tiago.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.tiago.entities.Order;
+import com.tiago.repositories.OrderRepository;
+
+@Service
+@Profile("products")
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll(){
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+	}
+}
