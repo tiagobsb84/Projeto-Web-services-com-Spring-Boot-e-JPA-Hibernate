948c3ff5af21d53ddcf294c5cbdcf0ec6d19445f
Tiago Silva de Oliveira <tiagobsb84@gmail.com>
2021-09-27 17:34:20 -0300

    Associacao de muitos para muitos com o JoinTable

--- src/main/java/com/tiago/resources/CategoryResource.java
+++ src/main/java/com/tiago/resources/CategoryResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +19,13 @@ public class CategoryResource {
 	@Autowired
 	private CategoryService categoryService;
 	
+	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
 		List<Category> list = categoryService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Category> findById(@PathVariable Long id){
 		Category obj = categoryService.findById(id);
 		return ResponseEntity.ok().body(obj);
