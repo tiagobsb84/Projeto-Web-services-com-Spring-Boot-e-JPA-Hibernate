061b19046425667385b0cc43582c55a4c9f05e73
Tiago Silva de Oliveira <tiagobsb84@gmail.com>
2021-09-30 09:07:07 -0300

    Entidade Payment, associacao de um para um com Entidade Order

--- src/main/java/com/tiago/config/TestConfig.java
+++ src/main/java/com/tiago/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.tiago.entities.Category;
 import com.tiago.entities.OrdemItem;
 import com.tiago.entities.Order;
+import com.tiago.entities.Payment;
 import com.tiago.entities.Product;
 import com.tiago.entities.User;
 import com.tiago.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		OrdemItem oi4 = new OrdemItem(o3, p5, 2, p5.getPrice()); 
 		
 		ordemItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T20:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
